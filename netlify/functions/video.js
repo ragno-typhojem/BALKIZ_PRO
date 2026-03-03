737a63bb3574ef3446679833398d08a6f61af2a6
Mustafa Berke Simsekler <ragnogamescorp@gmail.com>
2026-03-04 01:33:35 +0300

    test

--- netlify/functions/video.js
+++ netlify/functions/video.js
@@ -18,7 +18,7 @@ const videoRes = await fetch(
   {
     method: 'POST',
     headers: {
-      'Authorization': `Bearer ${HF_TOKEN}`,
+      'Authorization': `Bearer ${HF_API_KEY}`,
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ prompt: userPrompt })
